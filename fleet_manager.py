a52ad7d2d76ea5aaa82b1e05eadae49cc8f31342
OS <os13@kent.ac.uk>
2026-02-09 19:01:06 +0000

    menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,14 +4,30 @@ def main():
     print("there are {len(names)} members of the crew")
 
     while True:
-        print ("menu")
+        menu = display_menu() 
 
-        break
+        if menu == ("9"):
+            break
+        else:
+            print ("invalid option")
 
 def init_database():
     names = ["a","b","c","d","e"]
     ranks = ["rank1","rank2","rank3","rank4","rank5"]
     divs = ["div1","div2","div3","div4","div5"]
     ids = ["1","2","3","4","5"]
+    return names, ranks, divs, ids
+
+def display_menu():
+    print("1. Display members")
+    print("2. Add member")
+    print("3. Remove member")
+    print("4. Update ranks")
+    print("5. Search crew")
+    print("6. Filter by divs")
+    print("7. Payroll report")
+    print("8. Count officers")
+    print("9. End")
+    return input("Select option: ")
 
 main()
