4f088608f6313fd10140845fc97d9699068f71d9
OS <os13@kent.ac.uk>
2026-02-09 18:35:02 +0000

    first part

---
+++ fleet_manager.py
@@ -0,0 +1,17 @@
+def main():
+    names, ranks, divs, ids = init_database() 
+
+    print("there are {len(names)} members of the crew")
+
+    while True:
+        print ("menu")
+
+        break
+
+def init_database():
+    names = ["a","b","c","d","e"]
+    ranks = ["rank1","rank2","rank3","rank4","rank5"]
+    divs = ["div1","div2","div3","div4","div5"]
+    ids = ["1","2","3","4","5"]
+
+main()
