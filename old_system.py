c7d7ad31c42898a3a454effe0cc8ea948eaed23d
OS <os13@kent.ac.uk>
2026-02-09 17:51:00 +0000

    adds the rank and division to the lists

--- old_system.py
+++ old_system.py
@@ -35,7 +35,8 @@ def run_system_monolith():
             new_name = input("Name: ")
             new_rank = input("Rank: ")
             new_div = input("Division: ")
-            
+            r.append(new_rank)
+            d.append(new_div)
            
             n.append(new_name)
             print("Crew member added.")
