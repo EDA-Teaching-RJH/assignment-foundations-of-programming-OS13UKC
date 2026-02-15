d376d8b7ef512ad96bdd84831ac65575a264f222
OS <os13@kent.ac.uk>
2026-02-15 20:24:28 +0000

    addmember

--- fleet_manager.py
+++ fleet_manager.py
@@ -17,21 +17,43 @@ def display_menu():
     print("9. End")
     return input("Select option: ")
 
+def add_member(names, ranks, divs, ids):
+    print("Add new member")
+    new_id = input("Enter new ID")
+    
+    if new_id in ids:
+        print (new_id ,"already exists")
+        return
+        
+    print ("Ranks: rank1, rank2, rank3, rank4 and rank5")
+    new_rank = input("Enter rank")
+    valid_ranks = ["rank1","rank2","rank3","rank4","rank5"]
+    if new_rank not in valid_ranks:
+        print("Invalid rank try again")
+        return
+
+    new_name = input("Enter name")
+    new_div = input("Enter division")
+    
+
+    ids.append(new_id)
+    ranks.append(new_rank)
+    names.append(new_name)
+    divs.append(new_div)
+    print ("Crew member", new_name ,"added")
+
 def main():
     names, ranks, divs, ids = init_database() 
 
-    print("there are {len(names)} members of the crew")
+    print("there are" ,len(names), "members of the crew")
 
     while True:
         menu = display_menu() 
 
         if menu == ("9"):
             break
+        elif menu == "2":
+            add_member(names, ranks, divs, ids)
         else:
             print ("invalid option")
-
-def add_member(names, ranks, divs,ids):
-
-    pass
-
 main()
